94bd7189e4384f99ee3e42d647deca055358cabd
jaquelinenobremoratoreodonto-cpu <jaqueline.nobre.moratore.odonto@gmail.com>
2026-08-29 20:06:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // (Implantar > Nova implantação > Aplicativo da Web)
 // ============================================
 const CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/SUA_URL_DO_APPS_SCRIPT_AQUI/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw_nmvMGIajKNVcxTXpRtpYiSQDU7esuqn0nXywEMTHVUJzKT8JK9ypIkD2z2HzehwB/exec"
 };
